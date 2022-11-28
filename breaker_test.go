802c25f36789160a01dc5eca4ba4291278a05a61
Marcin Gorzynski <marcin@gorzynski.me>
2022-11-28 16:16:16 +0100

    Split to multiple test cases

--- breaker_test.go
+++ breaker_test.go
@@ -54,71 +54,79 @@ func TestBreakerDo(t *testing.T) {
 
 	br := New(mLog, 100*time.Millisecond, 2, 3)
 
-	// fails each time
-	mLog.On("Warnf", mock.Anything, mock.Anything).Return().Times(3)
-	mLog.On("Errorf", mock.Anything, mock.Anything).Return().Times(1)
+	t.Run("FailsEachTime", func(t *testing.T) {
+		// fails each time
+		mLog.On("Warnf", mock.Anything, mock.Anything).Return().Times(3)
+		mLog.On("Errorf", mock.Anything, mock.Anything).Return().Times(1)
 
-	err := br.Do(context.Background(), func() error {
-		return fmt.Errorf("error")
-	})
-	require.Error(t, err)
-	assert.True(t, errors.Is(err, ErrHitMaxRetries))
-
-	mLog.AssertExpectations(t)
-
-	// fails each time with long pause
-	mLog.On("Warnf", mock.Anything, mock.Anything).Return().Times(3)
-	mLog.On("Errorf", mock.Anything, mock.Anything).Return().Times(1)
-
-	err = br.Do(context.Background(), func() error {
-		time.Sleep(200 * time.Millisecond)
-		return fmt.Errorf("error")
-	})
-	require.Error(t, err)
-	assert.True(t, errors.Is(err, ErrHitMaxRetries))
-
-	mLog.AssertExpectations(t)
-
-	// doesn't fail
-	err = br.Do(context.Background(), func() error {
-		return nil
-	})
-	require.NoError(t, err)
-
-	mLog.AssertExpectations(t)
-
-	// fails one time
-	mLog.On("Warnf", mock.Anything, mock.Anything).Return().Once()
-
-	count := 0
-	err = br.Do(context.Background(), func() error {
-		defer func() { count++ }()
-
-		if count == 0 {
+		err := br.Do(context.Background(), func() error {
 			return fmt.Errorf("error")
-		}
-		return nil
+		})
+		require.Error(t, err)
+		assert.True(t, errors.Is(err, ErrHitMaxRetries))
+
+		mLog.AssertExpectations(t)
 	})
-	require.NoError(t, err)
 
-	mLog.AssertExpectations(t)
+	t.Run("FailsEachTimeWithLongWait", func(t *testing.T) {
+		mLog.On("Warnf", mock.Anything, mock.Anything).Return().Times(3)
+		mLog.On("Errorf", mock.Anything, mock.Anything).Return().Times(1)
 
-	// context canceled
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+		err := br.Do(context.Background(), func() error {
+			time.Sleep(200 * time.Millisecond)
+			return fmt.Errorf("error")
+		})
+		require.Error(t, err)
+		assert.True(t, errors.Is(err, ErrHitMaxRetries))
 
-	err = br.Do(ctx, func() error {
-		return nil
+		mLog.AssertExpectations(t)
 	})
-	require.Error(t, err)
 
-	mLog.AssertExpectations(t)
+	t.Run("DoesntFail", func(t *testing.T) {
+		err := br.Do(context.Background(), func() error {
+			return nil
+		})
+		require.NoError(t, err)
 
-	// ErrFatal break circuit right away
-	err = br.Do(context.Background(), func() error {
-		return superr.Wrap(ErrFatal, fmt.Errorf("error"))
+		mLog.AssertExpectations(t)
 	})
-	require.Error(t, err)
 
-	mLog.AssertExpectations(t)
+	t.Run("FailsOneTime", func(t *testing.T) {
+		mLog.On("Warnf", mock.Anything, mock.Anything).Return().Once()
+
+		count := 0
+		err := br.Do(context.Background(), func() error {
+			defer func() { count++ }()
+
+			if count == 0 {
+				return fmt.Errorf("error")
+			}
+			return nil
+		})
+		require.NoError(t, err)
+
+		mLog.AssertExpectations(t)
+	})
+
+	t.Run("FailsContextCanceled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		err := br.Do(ctx, func() error {
+			return nil
+		})
+		require.Error(t, err)
+
+		mLog.AssertExpectations(t)
+	})
+
+	t.Run("FailsErrFatal", func(t *testing.T) {
+		err := br.Do(context.Background(), func() error {
+			return superr.Wrap(ErrFatal, fmt.Errorf("error"))
+		})
+		require.Error(t, err)
+		assert.True(t, errors.Is(err, ErrFatal))
+
+		mLog.AssertExpectations(t)
+	})
 }
